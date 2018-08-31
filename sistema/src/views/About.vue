e4e201725fa85179faafb3a92c2bfb5c78daa21b
TiaMyllena <myllenah2o@gmail.com>
2018-08-31 11:26:06 -0300

    Bootstrap adicionado ao projeto

--- sistema/src/views/About.vue
+++ sistema/src/views/About.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    Nome: <input v-model="nomeInput" type="text">
+    <b-button variant="success" @click="adicionar()">Add</b-button>
+    <h1 v-for="nome in lista">{{nome}}</h1> 
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      nomeInput: '',
+      lista: []
+    }
+  },
+  methods: {
+    adicionar() {
+      this.lista.push(this.nomeInput)
+      this.nomeInput = ''
+    }
+  }
+}
+</script>
