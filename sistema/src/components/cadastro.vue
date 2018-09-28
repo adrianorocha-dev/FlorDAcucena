0ac7aaf9fb224465e506b0adeaaa7d9dbb399165
TiaMyllena <myllenah2o@gmail.com>
2018-09-28 14:49:59 -0300

    Botao de cadastrar adicionado a tela de cadastro

--- sistema/src/components/cadastro.vue
+++ sistema/src/components/cadastro.vue
@@ -1,66 +1,73 @@
 <template>
-    <div class="col-md-10">
-        <b-form>
-            <b-form-group id="nomeDoPedido"
+    <b-container>
+        <b-col md="8" offset-md="2">
+            <b-form>
+                <b-form-group id="nomeDoPedido"
                         label="Pedido:"
-                        label-for="PedidoInput2">
-            <b-form-input id="PedidoInput2"
+                        label-for="PedidoInput"
+                        horizontal>
+                    <b-form-input id="PedidoInput"
+                            type="text"
+                            required
+                            placeholder="">
+                    </b-form-input>
+                </b-form-group>
+
+                <b-form-group id="nomeCliente"
+                        label="Nome do Cliente:"
+                        label-for="nomeclienteInput3"
+                        horizontal>
+                    <b-form-input id="nomeClienteInput2"
+                        type="text"
+                        required
+                        placeholder="">
+                    </b-form-input>
+                </b-form-group>
+
+            <b-form-group id="descricao"
+                        label="Descrição:"
+                        label-for="exampleInput2"
+                        horizontal>
+                    
+                    <b-form-textarea id="textarea1"
+                        placeholder=""
+                        v-model="text"
+                        rows="3"
+                        max-rows="3">
+                    </b-form-textarea>
+        </b-form-group>
+
+            <b-form-group id="dataDeEntrega"
+                        label="Data de Entrega:"
+                        label-for="entregaInput4"
+                        horizontal>
+            <b-form-input id="entregaInput4"
                         type="text"
                         required
                         placeholder="">
             </b-form-input>
         </b-form-group>
 
-        <b-form-group id="nomeCliente"
-                    label="Nome do Cliente:"
-                    label-for="nomeclienteInput3">
-        <b-form-input id="nomeClienteInput2"
-                      type="text"
-                      required
-                      placeholder="">
-        </b-form-input>
-      </b-form-group>
-
-        <b-form-group id="descricao"
-                    label="Descrição:"
-                    label-for="exampleInput2">
-                
-                <b-form-textarea id="textarea1"
-                     placeholder=""
-                     v-model="text"
-                     rows="3"
-                     max-rows="6">
-                </b-form-textarea>
-      </b-form-group>
-
-        <b-form-group id="dataDeEntrega"
-                    label="Data de Entrega:"
-                    label-for="entregaInput4">
-        <b-form-input id="entregaInput4"
-                      type="text"
-                      required
-                      placeholder="">
-        </b-form-input>
-      </b-form-group>
-
-        <b-form-group id="tempoDeProducao"
-                    label="Tempo mínimo de produção:"
-                    label-for="tempoProducaoInput2">
-        <b-form-input id="tempoProducaoInput2"
-                      type="text"
-                      required
-                      placeholder="">
-        </b-form-input>
+            <b-form-group id="tempoDeProducao"
+                        label="Tempo mínimo de produção:"
+                        label-for="tempoProducaoInput2"
+                        horizontal>
+            <b-form-input id="tempoProducaoInput2"
+                        type="text"
+                        required
+                        placeholder="">
+            </b-form-input>
 
 
-      </b-form-group>
+        </b-form-group>
 
-       
-        
+            <b-button size="lg" variant="primary" >Cadastrar</b-button>
+            
 
 
-       </b-form> 
-    </div>
+        </b-form>
+        </b-col>
+    </b-container>
 </template>
 
 <script>
